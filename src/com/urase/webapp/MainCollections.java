7232d9466a0d2df220e867d623708730f7eeb232
SergeyLizunov <s.lizunov2020@yandex.ru>
2022-01-28 23:37:25 +0300

    lesson_8

--- src/com/urase/webapp/MainCollections.java
+++ src/com/urase/webapp/MainCollections.java
@@ -26,6 +26,7 @@ public class MainCollections {
         for (Resume r : collection) {
             System.out.println(r);
             if (Objects.equals(r.getUuid(), UUID_1)) {
+//                collection.remove(r);
             }
         }
 
